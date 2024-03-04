4359ad03ca51d940be72d62ffa4bdf135c014060
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-03-04 18:01:29 +0100

    Brough changes from main

--- player.h
+++ player.h
@@ -1,4 +1,14 @@
+#include <stdbool.h>
+#include "playingcard.h"
+
+/** \brief Represents a single player. */
 struct Player {
+    /** \brief Player's current amount of funds. If this number reaches 0, the player looses. */
     int funds;
+    /** \brief Tells if this player has already folded their cards in the current round. */
     bool folded;
-}
+    /** \brief A score table for individual hand ranks to help break up ties. See handranking.c for more info. */
+    int scores[10];
+    /** \brief Pointers to player's playing cards in his hand. */
+    struct PlayingCard* current_hand[2];
+};
