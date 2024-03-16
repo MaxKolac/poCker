77c9d2ab882eef0add5673a9dd00810c0a157c30
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-03-16 18:39:39 +0100

    Added guard macros

--- src/constants.h
+++ src/constants.h
@@ -1,3 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
 /**
 * \file     constants.h
 * \brief    Contains global constants that didn't fit other files.
@@ -8,3 +11,5 @@ const int COMM_CARDS_COUNT = 5;
 const int MIN_FUNDS_PER_PLAYER = 100;
 const int MAX_FUNDS_PER_PLAYER = 100000;
 const int PLAYER_COUNT = 4;
+
+#endif // CONSTANTS_H
