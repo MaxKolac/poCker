7d49025c42ad3f40e1276c5914171b059ad0e531
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-04-06 13:00:18 +0200

    Added tests for player

--- test/test_player.c
+++ test/test_player.c
@@ -1,13 +1,39 @@
-//HARD TODO
 #include "CuTest.h"
 #include "../src/player.h"
 
-void test_player_template(CuTest* ct){
-    CuAssert(ct, "", false);
+void test_player_checkConstructor(CuTest* ct){
+    Player* player = playerCreateNew();
+    CuAssert(ct, "Player starts with folded status", !player->folded);
+    CuAssert(ct, "Player starts with nonzero funds", player->funds == 0);
+    CuAssert(ct, "Player starts marked as human", !player->isHuman);
+    for (int i = 0; i < SCORE_TABLE_SIZE; ++i){
+        CuAssert(ct, "", player->scores[i] == 0);
+    }
+    for (int i = 0; i < CARDS_PER_PLAYER; ++i){
+        CuAssert(ct, "", player->current_hand[i] == NULL);
+    }
+}
+
+void test_player_checkResettingScoresWorks(CuTest* ct){
+    Player* player = playerCreateNew();
+    player->scores[0] = 1;
+    player->scores[1] = 100;
+    player->scores[4] = -100;
+    player->scores[5] = 9;
+    player->scores[6] = 42;
+    player->scores[8] = 190;
+    player->scores[9] = 1;
+
+    resetScores(player);
+
+    for (int i = 0; i < SCORE_TABLE_SIZE; ++i){
+        CuAssert(ct, "", player->scores[i] == 0);
+    }
 }
 
 CuSuite* PlayerGetSuite(CuTest* ct){
     CuSuite* suite = CuSuiteNew();
-    SUITE_ADD_TEST(suite, test_player_template);
+    SUITE_ADD_TEST(suite, test_player_checkConstructor);
+    SUITE_ADD_TEST(suite, test_player_checkResettingScoresWorks);
     return suite;
 }
