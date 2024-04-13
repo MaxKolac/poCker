440571a8d35289ef2dbf8984babe7b65517c4b7a
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-04-13 21:34:28 +0200

    Integrated playingcard_enums into playingcards
    This should have been done long time ago when I discovered guard macros...

--- src/playingcard.h
+++ src/playingcard.h
@@ -2,7 +2,6 @@
 #define PLAYINGCARD_H
 
 #include <string.h>
-#include "playingcard_enums.h"
 
 /**
 * \file     playingcard.h
@@ -16,6 +15,42 @@
 extern const int PIPS_PER_SUIT_OBJ;
 extern const int SUITS_COUNT_OBJ;
 extern const int DECK_LENGTH_OBJ;
+
+/** \brief Represents a symbol in a card deck. */
+enum Suit {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+};
+
+/**
+ *  \brief Represents the amount of symbols on a card and more importantly, its value amongst other cards.
+ *  \warning Keep in mind that for the sake of hand rank score calculation, Pip enumerator starts with 1, and not zero!
+ */
+enum Pip {
+    TWO = 1,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
+
+/** \brief Represents a single playing card. */
+typedef struct {
+    /** \brief The card's symbol. */
+    enum Suit suit;
+    /** \brief The amount of symbols on a card. */
+    enum Pip pips;
+} PlayingCard;
 
 const char* getPipName(enum Pip);
 const char* getSuitName(enum Suit);
