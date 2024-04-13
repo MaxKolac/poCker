4def6e472a143855db67ffc2c02c01d8474b7a69
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-04-13 15:10:01 +0200

    Added some docs

--- src/gamestate.h
+++ src/gamestate.h
@@ -9,19 +9,38 @@
 
 #define MAX_BETS_PER_ROUND 3
 
+/**
+ *  \file gamestate.h
+ *  \brief Contains the logic of a poker game in a form of DI'able functions. See their arrangement and implementation in main.c.
+ */
+
 extern const int MAX_BETS_PER_ROUND_OBJ;
 
+/**
+ *  \brief Represents the current state of a poker game.
+ *  Recommended to initialize this struct through gsCreateNew(), but hey I'm not your mom.
+ */
 typedef struct {
+    /** \brief Amount of community cards which are "face-up" meaning visible to players. */
     int revealed_comm_cards;
-    int dealer_player;
+    /** \brief Index of the player with dealer's button. */    int dealer_player;
+    /** \brief Index of the player with "small blind" status. */
     int s_blind_player;
+    /** \brief Index of the player with "big blind" status. */
     int b_blind_player;
+    /** \brief Index of the player who currently has a chance to act. */
     int current_player;
+    /** \brief Number of the betting round. There are 4 rounds: pre-flop, flop, turn and river. */
     int betting_round;
+    /** \brief How many turns are left until the current betting round ends. */
     int turns_left;
+    /** \brief How many raises have been called by all players in the current betting round. */
     int raises_performed;
+    /** \brief Boolean to indicate if all but one player have folded their cards - this indicates an auto-win for that player. */
     bool all_but_one_folded;
+    /** \brief Amount currently in the pot. */
     unsigned int pot;
+    /** \brief Amount of the current bet. */
     unsigned int bet;
 } GameState;
 
